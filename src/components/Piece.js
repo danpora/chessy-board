b40698a81ee7946475a19e8f891c9d658e5fff23
danpora <danpora@post.bgu.ac.il>
2019-02-03 23:25:58 +0200

    import utils, add role and color validators, fast return if no elements exists

--- src/components/Piece.js
+++ src/components/Piece.js
@@ -1,20 +1,20 @@
 import React from 'react';
+import { isColorValid, isRoleValid } from '../utils';
 
 export function Piece (props) {
   const { elements } = props
-  
+
+  if (!elements) return null;
+
+  if (
+    !isColorValid(elements.color) || !isRoleValid(elements.role)
+  ) return null;
+
   return (
-    elements
-      ? <img 
-          alt={elements.role}
-          style={{ width: '100%' }}
-          src={require(`../assets/images/pieces/${elements.color}_${elements.role}.png`)}
-        />
-      : null
+    <img 
+      alt={elements.role}
+      style={{ width: '100%' }}
+      src={require(`../assets/images/pieces/${elements.color}_${elements.role}.png`)}
+    />
   )
 }
-
-export const getPieceState = (role, color) => ({
-  role,
-  color
-});
